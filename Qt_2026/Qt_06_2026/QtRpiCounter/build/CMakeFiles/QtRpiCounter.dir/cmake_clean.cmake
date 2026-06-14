f035ba547b624298fabcb97a121e48bca52d1eb7
BexelOJ <bexelo.j@outlook.com>
2026-06-15 00:44:33 +0530

    adding to exinRpiSer

---
+++ Qt_2026/Qt_06_2026/QtRpiCounter/build/CMakeFiles/QtRpiCounter.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/QtRpiCounter_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/QtRpiCounter_autogen.dir/ParseCache.txt"
+  "QtRpiCounter_autogen"
+  "CMakeFiles/QtRpiCounter.dir/QtRpiCounter_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/QtRpiCounter.dir/QtRpiCounter_autogen/mocs_compilation.cpp.o.d"
+  "CMakeFiles/QtRpiCounter.dir/main.cpp.o"
+  "CMakeFiles/QtRpiCounter.dir/main.cpp.o.d"
+  "CMakeFiles/QtRpiCounter.dir/mainwindow.cpp.o"
+  "CMakeFiles/QtRpiCounter.dir/mainwindow.cpp.o.d"
+  "QtRpiCounter"
+  "QtRpiCounter.pdb"
+  "QtRpiCounter_autogen/mocs_compilation.cpp"
+  "QtRpiCounter_autogen/timestamp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/QtRpiCounter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Qt_2026/Qt_06_2026/QtRpiCounter/build/CMakeFiles/QtRpiCounter_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/QtRpiCounter_autogen"
+  "QtRpiCounter_autogen/include/ui_mainwindow.h"
+  "QtRpiCounter_autogen/mocs_compilation.cpp"
+  "QtRpiCounter_autogen/timestamp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/QtRpiCounter_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
